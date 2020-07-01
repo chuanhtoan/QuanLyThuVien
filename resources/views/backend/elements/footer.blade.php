425c53606f8925598fe88eba76a68099875a9042
chuanhtoan <chuanhtoan139@gmail.com>
2020-07-01 11:10:49 +0700

    Update footer.blade.php

--- resources/views/backend/elements/footer.blade.php
+++ resources/views/backend/elements/footer.blade.php
@@ -4,7 +4,7 @@
 <!-- ============================================================== -->
 <!-- All Jquery -->
 <!-- ============================================================== -->
-<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="{{asset('backend/html/bootstrap/dist/js/bootstrap.min.js')}}"></script>
 {{-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script> --}}
 <!-- Menu Plugin JavaScript -->
